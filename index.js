8e74ba66fb0908486f40e02d8b94bce027b610a1
Kristian Ramirez <Kristian@sids-MacBook-Pro-2.local>
2023-06-24 17:50:19 -0500

    updated index.js and shapes.js. Checking if the logo will open in browser

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const inquirer = require("inquirer");
 const fs = require("fs");
 //require classes from shapes.js
-
+const { Circle, Triangle, Square } = require("./lib/shapes");
 
 
 //prompt user to answer questions
@@ -10,31 +10,57 @@ function userPrompts() {
         .prompt([
             {
                 type: "input",
-                message: "Enter three characters for your logo."
-            name: "text",
+                message: "Enter three characters for your logo.",
+                name: "text",
             },
             //text color prompt
             {
                 type: "input",
-                message: "What color would you like your text to be? Enter color OR hexadecimal number."
-            name: "textColor",
+                message: "What color would you like your text to be? Enter color OR hexadecimal number.",
+                name: "textColor",
             },
             //shape prompt
             {
                 type: "list",
-                message: "Pick a shape for your logo."
-            choices: ["Circle", "Triangle", "Square"]
-            name: "shape",
+                message: "Pick a shape for your logo.",
+                choices: ["Circle", "Triangle", "Square"],
+                name: "shape",
             },
             //shape color prompt
             {
                 type: "input",
-                message: "What color would you like your shape to be? Enter color OR hexadecimal number."
-            name: "shapeColor",
+                message: "What color would you like your shape to be? Enter color OR hexadecimal number.",
+                name: "shapeColor",
             },
         ])
         .then((answers) => {
-            fs.writeFile("logo.svg", answers);
+            let shape;
+
+            switch (answers.shape) {
+                case "Circle":
+                    shape = new Circle();
+                    break;
+                case "Triangle":
+                    shape = new Triangle();
+                    break;
+                case "Square":
+                    shape = new Square();
+                    break;
+            }
+
+            shape.text = answers.text;
+            shape.textColor = answers.textColor;
+            shape.shapeColor = answers.shapeColor;
+
+            const svgString = shape.render();
+
+            fs.writeFile("logo.svg", svgString, (err) => {
+                if (err) {
+                    console.error("Error writing SVG file:", err);
+                } else {
+                    console.log("Generated logo.svg");
+                }
+            });
         });
 }
 
